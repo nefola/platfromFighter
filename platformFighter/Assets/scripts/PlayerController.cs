27672a97026efe2bdc5bc67c388be494de2bd8a4
Astral Abrams-Greenberg <nefolaC4@gmail.com>
2021-05-14 11:39:06 -0400

    5/14/21
    - functioning grounded status
    - proper air jump management

--- platformFighter/Assets/scripts/PlayerController.cs
+++ platformFighter/Assets/scripts/PlayerController.cs
@@ -3,21 +3,28 @@ using UnityEngine;
 // player controller currently based off of tutorial 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    public PlayerManger playerManger;
 
     public bool beLeft;
     public bool beRight;
-
+    bool hasAirJumps;
+    bool amGrounded;
+    public bool tryJumping = false;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        rb = GetComponent<Rigidbody2D>();
+        playerManger = GetComponent<PlayerManger>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        //getting info from player manger
+        hasAirJumps = playerManger.hasAirJumps;
+        amGrounded = playerManger.amGrounded;
         //moves to the left
         if (Input.GetKey(KeyCode.L))
         {
@@ -31,11 +38,16 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector2(5, rb.velocity.y);
             beRight = true;
             beLeft = false;
+            
         }
         // jump code
-        if(Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.C) && (amGrounded || hasAirJumps))
         {
             rb.velocity = new Vector2(rb.velocity.x, 15f);
+            tryJumping = true;
+        }  else
+        {
+            tryJumping = false;
         }
 
     }
--- platformFighter/Assets/scripts/PlayerManger.cs
+++ platformFighter/Assets/scripts/PlayerManger.cs
@@ -8,30 +8,49 @@ public class PlayerManger : MonoBehaviour
 {
     public PlayerController playerController;
     public Transform transfrom;
-
+    private Rigidbody2D rb;
+    private Collider2D coll;
     public bool amLeft = false;
     public bool amRight = true;
     public bool amGrounded = true;
+    public bool hasAirJumps;
+    bool amJumping;
+    public int airJumps = 3;
+    [SerializeField] private LayerMask platfrom; //done for more efficent processing
+    //private enum status {worda, wordb, wordc};
+    //possible way of loging status more effectivly 
   
     // Start is called before the first frame update
     void Start()
     {
         // lets us import from a PlayerController
-        playerController = GetComponent<PlayerController>(); 
-    }
-
-    void LateUpdate()
-    {
+        playerController = GetComponent<PlayerController>();
+        coll = GetComponent<Collider2D>();
         
     }
+
     // Update is called once per frame
     void Update()
     {
         //importing direction from playerControler
         amLeft = playerController.beLeft;
         amRight = playerController.beRight;
+        amJumping = playerController.tryJumping;
+        //float hDir = Input.GetAxis("Horizontal");
+        //used for unity side controls
 
-        if(amLeft == true)
+        if (coll.IsTouchingLayers(platfrom))      // the platform layer
+        {
+            amGrounded = true;
+        }   
+        else
+        {
+            amGrounded = false;
+        }
+        print(amGrounded); //debug line
+        // why the fuck am i not grounded
+
+        if (amLeft == true)
         {
             transform.localScale = new Vector2 (-1, 1);
         }
@@ -39,6 +58,23 @@ public class PlayerManger : MonoBehaviour
         {
             transform.localScale = new Vector2(1, 1);
         }
+        //refreshes jumps on ground, subject to change
+        if(amGrounded == true)
+        {
+            airJumps = 3;
+        }
+        if(airJumps > 0)
+        {
+            hasAirJumps = true;
+        } else
+        {
+            hasAirJumps = false;
+        }
+        if (amJumping == true)
+        {
+            airJumps--;
+        }
+
 
     }
 }
